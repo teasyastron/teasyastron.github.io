46e79e91ca4f38492ba73577bf3c71f21173888c
John Doe <johndoe@email.com>
2024-07-14 00:28:39 +0200

    Twitch Planning

---
+++ static/js/462.d85c6417.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktwitch_planning=self.webpackChunktwitch_planning||[]).push([[462],{462:(t,a,l)=>{l.d(a,{zDP:()=>e});var n=l(441);function e(t){return(0,n.k5)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=462.d85c6417.chunk.js.map
